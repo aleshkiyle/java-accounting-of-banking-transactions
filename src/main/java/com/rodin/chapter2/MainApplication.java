23184a1aebd564c464ab666ca03f9333e2b98f84
aleshkiyle <aleksey.rodin62@gmail.com>
2023-01-31 11:44:46 +0300

    using a CSV parser and moving the counting logic into separate methods

--- src/main/java/com/rodin/chapter2/MainApplication.java
+++ src/main/java/com/rodin/chapter2/MainApplication.java
@@ -14,25 +14,32 @@ public class MainApplication {
     private static final String CSV_SOURCE = "src/main/resources/bank-simple.csv";
 
     public static void main(String[] args) throws IOException {
+
+        final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
+
         final Path path = Path.of(CSV_SOURCE);
         final List<String> lines = Files.readAllLines(path);
-        double total = 0d;
-        for (String line: lines) {
-            String[] columns = line.split(",");
-            double amount = Double.parseDouble(columns[1]);
-            total += amount;
-        }
-        System.out.printf("The total for all transactions is %.1f%n", total);
+        final List<BankTransaction> bankTransactions = bankStatementCSVParser.parseLineFromCSVFile(lines);
 
-        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        double totalInJanuary = 0d;
-        for (String line: lines) {
-            String[] columns = line.split(",");
-            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
-            if (date.getMonth() == Month.JANUARY) {
-                totalInJanuary += Double.parseDouble(columns[1]);
-            }
-        }
-        System.out.printf("The total for all transactions in January is %.1f", totalInJanuary);
+        System.out.printf("The total for all transactions is %.1f%n",
+                calculateTotalAmount(bankTransactions));
+        System.out.printf("The total for all transactions in January is %.1f",
+                selectInMont(bankTransactions, Month.JANUARY));
+    }
+
+    private static double calculateTotalAmount(final List<BankTransaction> bankTransactions) {
+        return bankTransactions.stream()
+                .map(BankTransaction::amount)
+                .reduce(Double::sum)
+                .orElse(0d);
+    }
+
+    private static double selectInMont(final List<BankTransaction> bankTransactions,
+                                       final Month month) {
+        return bankTransactions.stream()
+                .filter(bankTransaction -> bankTransaction.date().getMonth() == month)
+                .map(BankTransaction::amount)
+                .reduce(Double::sum)
+                .orElse(0d);
     }
 }
