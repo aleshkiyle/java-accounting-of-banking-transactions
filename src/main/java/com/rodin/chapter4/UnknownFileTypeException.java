865b391a9627fef0d6ac01ca0262f0ece8553bbf
aleshkiyle <aleksey.rodin62@gmail.com>
2023-02-01 11:50:02 +0300

    add new UnknownFileTypeException

---
+++ src/main/java/com/rodin/chapter4/UnknownFileTypeException.java
@@ -0,0 +1,8 @@
+package com.rodin.chapter4;
+
+public class UnknownFileTypeException extends RuntimeException {
+
+    public UnknownFileTypeException(final String message) {
+        super(message);
+    }
+}
